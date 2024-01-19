da385b6f6539434b272bedf17d94942aeb53dfb4
mListek <michal.listex@gmail.com>
2024-01-19 10:52:32 +0100

    added more dummy data

--- components/Expenses/ExpensesOutput.js
+++ components/Expenses/ExpensesOutput.js
@@ -34,6 +34,36 @@ const DUMMY_EXPENSES = [
     amount: 20.0,
     date: new Date("2022-11-29"),
   },
+  {
+    id: "e6",
+    description: "A pair of shoes",
+    amount: 59.99,
+    date: new Date("2024-01-07"),
+  },
+  {
+    id: "e7",
+    description: "A pair of trousers",
+    amount: 89.98,
+    date: new Date("2023-11-17"),
+  },
+  {
+    id: "e8",
+    description: "Some bananas",
+    amount: 5.99,
+    date: new Date("2024-01-02"),
+  },
+  {
+    id: "e9",
+    description: "A book",
+    amount: 14.5,
+    date: new Date("2023-11-27"),
+  },
+  {
+    id: "e10",
+    description: "Another book",
+    amount: 20.0,
+    date: new Date("2022-11-29"),
+  },
 ];
 
 const ExpensesOutput = ({ expenses, expensesPeriod }) => {
@@ -50,7 +80,8 @@ export default ExpensesOutput;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 24,
+    paddingHorizontal: 24,
+    paddingTop: 24,
     backgroundColor: GlobalStyles.colors.primary700,
   },
 });
